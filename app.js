def4c46083890d00af88576b73536bc3f92ae035
Andres Alba <andres_aalba@hotmail.com>
2019-03-09 11:55:21 -0500

    Update request options and URL query string parameters (Lecture 32)

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/fc2044bdaa450ea332cdf82740665a01/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/fc2044bdaa450ea332cdf82740665a01/37.8267,-122.4233?units=si&lang=es';
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({
+    url: url,
+    json: true
+}, (error, response) => {
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + ' % chance of rain');
 });
