4da61d1c42f2ad131e6e8a8ebc438dcbad86b5e7
Andres Alba <andres_aalba@hotmail.com>
2019-03-09 12:26:22 -0500

    Add requests error handling (Lecture 34)

--- app.js
+++ app.js
@@ -6,16 +6,29 @@ request({
     url: url,
     json: true
 }, (error, response) => {
-    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + ' % chance of rain');
+    if (error) {
+        console.log('Unable to connect to weather service!');
+    } else if (response.body.error) {
+        console.log('Unable to find location');
+    } else {
+        console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + ' % chance of rain');
+    }
 });
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWFsYmE5MyIsImEiOiJjanQxcWtlcjgwZXkxNDRtbzBiZXI1cDM3In0.xRXvvJtUvEJL4tFdERfp7Q&limit=1';
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Bogota.json?access_token=pk.eyJ1IjoiYWFsYmE5MyIsImEiOiJjanQxcWtlcjgwZXkxNDRtbzBiZXI1cDM3In0.xRXvvJtUvEJL4tFdERfp7Q&limit=1';
 
 request({
     url: geocodeURL,
     json: true
 }, (error, response) => {
-    const latitue = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(latitue, longitude);
+    if (error) {
+        console.log('Unable to connect to location services!');
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search');
+    } else {
+        const latitue = response.body.features[0].center[1];
+        const longitude = response.body.features[0].center[0];
+        console.log(latitue, longitude);
+    }
+    
 });
