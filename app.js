611545eb2e7b6a4801f5cafb514a03430de1a322
Andres Alba <andres_aalba@hotmail.com>
2019-03-12 21:37:53 -0500

    Add address/location console user input (Lecture 38)

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Boston', (error, data) => {
-    if (error) {
-        return console.log('Error', error);
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+const address = process.argv[2];
+
+if (!address) {
+    console.log('Please provide an address');
+} else {
+    geocode(address, (error, data) => {
         if (error) {
             return console.log('Error', error);
         }
-        console.log(data.location);
-        console.log(forecastData);
-      });
-});
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log('Error', error);
+            }
+            console.log(data.location);
+            console.log(forecastData);
+          });
+    });
+}
+
 
